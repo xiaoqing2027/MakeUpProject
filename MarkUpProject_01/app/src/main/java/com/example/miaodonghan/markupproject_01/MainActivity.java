5f728ca7d44c1a001af25f3e708e0bfd3522c3e2
maggie <maggie94gkn@gmail.com>
2016-02-07 22:50:45 -0600

    add real time edit functionality

--- MarkUpProject_01/app/src/main/java/com/example/miaodonghan/markupproject_01/MainActivity.java
+++ MarkUpProject_01/app/src/main/java/com/example/miaodonghan/markupproject_01/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.ClipDescription;
 import android.provider.DocumentsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.DragEvent;
 import android.view.MotionEvent;
@@ -46,15 +48,41 @@ public class MainActivity extends AppCompatActivity {
         headerbtn.setOnTouchListener(new MyTouchListener());
         eText.setOnDragListener(new MyDragListener());
 
+
+        eText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                //Your query to fetch Data
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() > 0) {
+
+                    //Your query to fetch Data
+
+                    MarkdownView markdownView = (MarkdownView) findViewById(R.id.markdownView);
+                    markdownView.loadMarkdown(eText.getText().toString());
+                }
+            }
+        });
+
 //        doc = new Document(text);
 //
 //        doc.setHeader(6);
 //        doc.setBold(7);
 
-//        MarkdownView markdownView = (MarkdownView) findViewById(R.id.markdownView);
-//        markdownView.loadMarkdown("## Hello Markdown");
 
     }
+
+
 
     private final class MyTouchListener implements OnTouchListener {
         public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -71,7 +99,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     class MyDragListener implements OnDragListener {
-
 
         @Override
         public boolean onDrag(View v, DragEvent event) {
@@ -90,16 +117,10 @@ public class MainActivity extends AppCompatActivity {
                 case DragEvent.ACTION_DROP:
 
                     // Dropped, reassign View to ViewGroup
-
                     Toast.makeText(MainActivity.this,"drag drop", Toast.LENGTH_SHORT).show();
                     break;
                 case DragEvent.ACTION_DRAG_ENDED:
-                    MarkdownView markdownView = (MarkdownView) findViewById(R.id.markdownView);
-                    String t = eText.getText().toString();
-                  
-                    eText.setText("# " + t);
-                    markdownView.loadMarkdown(eText.getText().toString());
-                    Toast.makeText(MainActivity.this,"drag end", Toast.LENGTH_SHORT).show();
+                    displayAndCheckMarkDownFormat();
                     break;
                 default:
                     break;
@@ -108,8 +129,34 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void displayAndCheckMarkDownFormat(){
+        MarkdownView markdownView = (MarkdownView) findViewById(R.id.markdownView);
+        String t = eText.getText().toString();
+        if(t.charAt(0) != '#'){
+            eText.setText("# " + t);
+        }else if(t.charAt(1) != '#' || t.charAt(2) != '#' ){
+            eText.setText("#" + t);
+        }else if(t.charAt(2) == '#'){
+            eText.setText(t.substring(4,t.length()));
+        }
+        markdownView.loadMarkdown(eText.getText().toString());
+    }
+
     public void onTouch(View v, MotionEvent e){
         Toast.makeText(MainActivity.this, "display text", Toast.LENGTH_LONG).show();
+
+        eText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+    /* When focus is lost check that the text field has valid values.*/
+                if (!hasFocus) {
+                    //validateInput(v);
+                    MarkdownView markdownView = (MarkdownView) findViewById(R.id.markdownView);
+                    markdownView.loadMarkdown(eText.getText().toString());
+                }
+            }
+        });
     }
 
 
